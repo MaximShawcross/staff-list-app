f7d24a9430f5f123c12bb4e80b1f492a3ceaca99
Maxim <maxbasta2006@gmail.com>
2022-03-01 02:51:27 +0200

    structure is done

---
+++ src/components/app/app.js
@@ -0,0 +1,19 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-lifter/app-filter';
+import './app.css';
+
+
+function App(){
+    return(<div className="app">
+        <AppInfo/>
+
+        <div className="search-panel">
+            <SearchPanel/>
+
+            <AppFilter/>
+        </div>
+    </div>)
+}
+
+export default App;
